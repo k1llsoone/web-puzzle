57501c4ca7cb6f083c28acfecb9dccc8dfc11586
lovelyugly <k1llsoone23@yandex.ru>
2022-08-16 19:05:49 +0300

    v 1.0.0

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Project X</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('body').css('background','black').delay(3000);
+	});
+</script>
+</body>
+</html>
